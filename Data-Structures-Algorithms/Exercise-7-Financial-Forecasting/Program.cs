608243451cdf1dc52b4b8938a53e6356fd2bc81d
manvitha4786 <231fa04786@gmail.com>
2026-07-23 21:55:02 +0530

    Add files via upload

---
+++ Data-Structures-Algorithms/Exercise-7-Financial-Forecasting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    // Recursive method to calculate future value
+    static double FutureValue(double currentValue, double growthRate, int years)
+    {
+        // Base Case
+        if (years == 0)
+        {
+            return currentValue;
+        }
+
+        // Recursive Call
+        return FutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        double presentValue = 10000;
+        double growthRate = 0.10;   //10%
+        int years = 5;
+
+        double future = FutureValue(presentValue, growthRate, years);
+
+        Console.WriteLine("Present Value : " + presentValue);
+        Console.WriteLine("Growth Rate   : " + (growthRate * 100) + "%");
+        Console.WriteLine("Years         : " + years);
+        Console.WriteLine("Future Value  : " + future);
+    }
+}
