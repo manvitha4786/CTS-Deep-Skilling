1edb4f0d97091c10f16e5840556c0f26a4c34a1c
manvitha4786 <231fa04786@gmail.com>
2026-07-25 20:58:09 +0530

    Add files via upload

---
+++ NUnit-Moq/NUnit-Handson/CalculatorTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private Calculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new Calculator();
+    }
+
+    [TestCase(2, 3, 5)]
+    [TestCase(10, 20, 30)]
+    [TestCase(5, 7, 12)]
+    public void Add_ReturnsExpectedResult(int a, int b, int expected)
+    {
+        Assert.That(calculator.Add(a, b), Is.EqualTo(expected));
+    }
+}
